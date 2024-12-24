5657c90be3266e43094ce71d4e335d0bcd9425a0
moram98 <moram7992@gmail.com>
2024-12-24 10:40:25 -0500

    primera actulizacion

---
+++ src/main/java/com/literalura/literalura/repository/LibroRepository.java
@@ -0,0 +1,26 @@
+package com.literalura.literalura.repository;
+
+
+import com.literalura.literalura.model.Autor;
+import com.literalura.literalura.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+    Libro findByTitulo(String titulo);
+
+
+    List<Libro> findByIdiomasContaining(String idioma);
+
+    @Query("SELECT COUNT(l) FROM Libro l WHERE l.idiomas = :idiomas")
+    int countByLanguage(@Param("idiomas") String idiomas);
+
+    List<Libro> findTop10ByOrderByNumeroDescargasDesc();
+
+    List<Libro> findByAutor(Autor autor);
+
+
+}
